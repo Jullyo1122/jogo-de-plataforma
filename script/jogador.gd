d5c42561ba86ae80e870870a2255ddb0bcb0369f
Jullyo <jullyoc74@gmail.com>
2026-05-08 19:14:31 -0300

    criando camera do jogador

--- script/jogador.gd
+++ script/jogador.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 500.0
+const JUMP_VELOCITY = -600.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	 # Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
