f7160106478c13d334c60a5fbc66dbaf44961d0b
Jullyo <jullyoc74@gmail.com>
2026-06-15 19:55:07 -0300

    melhorando estrutura do script

--- entities/player_2.gd
+++ entities/player_2.gd
@@ -1,40 +1,88 @@
 extends CharacterBody2D
+
+
+enum PlayerState{
+	idle,
+	run,
+	jump
+}
 @onready var anim: AnimatedSprite2D = $AnimacaoGirl
 
-
 const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -300.0
 
+var status = PlayerState
+
+func _ready() -> void:
+	go_to_idle_state()
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+		
+	match status:
+		
+		PlayerState.idle:
+			idle_state()
+		PlayerState.run:
+			run_state()
+		PlayerState.jump:
+			jump_state()
+			
+	move_and_slide()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func go_to_idle_state():
+	status = PlayerState.idle
+	anim.play("idle")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+func go_to_run_state():
+	status = PlayerState.run
+	anim.play("run")
+	
+func go_to_jump_state():
+	status = PlayerState.jump
+	anim.play("jump")
+	velocity.y = JUMP_VELOCITY
+	
+func idle_state():
+	move()
+	if velocity.x != 0:
+		go_to_run_state()
+	
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+	
+
+func run_state():
+	move()
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+	
+
+func jump_state():
+	move()
+	if is_on_floor():
+		if velocity.x == 0:
+			go_to_idle_state()
+		else:
+			go_to_run_state()
+		return
+
+func move():
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	if is_on_floor():
-			
-		if direction > 0:
-			anim.flip_h = false
-			anim.play("Run")
-
-		elif direction < 0:
-			anim.flip_h = true
-			anim.play("Run")
-		else:
-			anim.play("Idle")
-	else:
-		anim.play("Jump")
-
-	move_and_slide()
+	if direction < 0:
+		anim.flip_h = true 
+	elif direction > 0:
+		anim.flip_h = false
