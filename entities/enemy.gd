36f6a9bdfc8f2eaca46c9364668cf1d3b7147075
Jullyo <jullyoc74@gmail.com>
2026-06-30 16:12:58 -0300

    adicionando animação de ataque do inimigo

--- entities/enemy.gd
+++ entities/enemy.gd
@@ -5,9 +5,12 @@ var amplitude = 20
 var frequency = 2.0
 var time = 0.0
 
+@onready var anim: AnimatedSprite2D = $Animacaobat
+
 enum BatState {
 	PATROL,
-	ATTACK
+	ATTACK,
+	COOLDOWN
 }
 
 var state = BatState.PATROL
@@ -21,20 +24,24 @@ func _physics_process(delta):
 
 		BatState.ATTACK:
 			attack()
+		
+		BatState.COOLDOWN:
+			cooldown(delta)
 	update_sprite_direction()
 	move_and_slide()
 
 func update_sprite_direction():
 	if velocity.x > 0:
-		$AnimatedSprite2D.flip_h = false
+		$Animacaobat.flip_h = false
 	elif velocity.x < 0:
-		$AnimatedSprite2D.flip_h = true
+		$Animacaobat.flip_h = true
 		
 func patrol(delta):
 	time += delta
 
 	velocity.x = -speed
 	velocity.y = sin(time * frequency) * amplitude
+	anim.play("patrol")
 	
 func _on_detection_area_body_entered(body):
 	print("Entrou:", body.name)
@@ -44,6 +51,7 @@ func _on_detection_area_body_entered(body):
 		print("Jogador detectado")
 		player = body
 		state = BatState.ATTACK
+		anim.play("attack")
 
 
 func _on_detection_area_body_exited(body):
@@ -52,16 +60,31 @@ func _on_detection_area_body_exited(body):
 		state = BatState.PATROL
 		
 var attack_speed = 200
+@export var attack_cooldown := 1.5
 
 func attack():
 	print("Atacando")
 	
 	if player == null:
 		return
-
+		
+	
+	$AttackCooldown.start()
+	
 	var direction = (player.global_position - global_position).normalized()
-
 	velocity = direction * attack_speed
-
+	
+	state = BatState.COOLDOWN
+	
+func cooldown(delta):
+	pass
+	
 func _ready():
 	$HitBox.add_to_group("enemy_attack")
+
+
+func _on_attack_cooldown_timeout() -> void:
+	if player != null:
+		state = BatState.ATTACK
+	else:
+		state = BatState.PATROL
