31137f05e2258df94f2b9afaa730313e14143aa6
Jullyo <jullyoc74@gmail.com>
2026-08-12 22:12:49 -0300

    ainda tentando resolver o bug de não trocar o ataque de lado e falhando miseravelmente dnv

--- entities/abobora.gd
+++ entities/abobora.gd
@@ -6,6 +6,8 @@ const SPEED = 100.0
 @onready var detection: Area2D = $DetectionArea
 @onready var attack_timer: Timer = $AttackTimer
 @onready var wall_detector: RayCast2D = $WallDetector
+@onready var attack_hitbox: CollisionShape2D = $Hitbox/CollisionShape2D
+
 enum AboboraState {
 	IDLE,
 	PATROL,
@@ -68,17 +70,30 @@ func change_state(new_state):
 			attack_timer.start(0.8)
 
 		AboboraState.ATTACK:
-			# É AQUI
 			if player:
-				attack_direction = sign(
-					player.global_position.x - global_position.x
-				)
-
-				anim.flip_h = attack_direction < 0
-
+				print("Posição Player: ", player.global_position.x, " | Posição Inimigo: ", global_position.x)
+				
+				# Checagem literal e super simples:
+				if player.global_position.x < global_position.x:
+					print("Decisão: Player está na ESQUERDA")
+					anim.flip_h = true
+					$Hitbox.position.x = -30.0 # Move a Area2D inteira para a esquerda
+				else:
+					print("Decisão: Player está na DIREITA")
+					anim.flip_h = false
+					$Hitbox.position.x = 30.0  # Move a Area2D inteira para a direita
+				
 			anim.play("attack")
-
-
+			
+			await get_tree().create_timer(1.0).timeout
+			
+			if state != AboboraState.ATTACK:
+				return
+				
+			attack_hitbox.set_deferred("disabled", false)
+			await get_tree().create_timer(0.55).timeout
+			attack_hitbox.set_deferred("disabled", true)
+			
 func _on_body_entered(body):
 
 	if body.is_in_group("player"):
